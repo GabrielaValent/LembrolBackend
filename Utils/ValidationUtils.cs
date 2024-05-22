e4dddc33e94932c0f23e4c5cd38dff6318835d85
GabrielaValent <gabrielalimborco@gmail.com>
2024-05-22 16:33:58 -0300

    feat: add validation utils

---
+++ Utils/ValidationUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace backend_lembrol.Utils
+{
+    public static class ValidationUtils
+    {
+        public static void ValidateIntDayOfWeek(int dayDto)
+        {
+            if (dayDto < 0 || dayDto >= 7)
+            {
+                throw new ArgumentOutOfRangeException(nameof(dayDto), dayDto, "Day must be between 0 and 6.");
+            }
+        }
+    }
+}
